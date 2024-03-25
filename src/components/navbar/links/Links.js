be161fda85e174a24e6874791fa1db443d5195dd
Fahad Ali <fahad.ali@appinsnap.com>
2024-03-25 12:01:37 +0500

    navigation

---
+++ src/components/navbar/links/Links.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const linls = [
+    {
+        title: 'Homepage',
+        path: '/'
+    },
+    {
+        title: 'About',
+        path: '/about'
+    },
+    {
+        title: 'Contact',
+        path: '/contact'
+    },
+    {
+        title: 'Blog',
+        path: '/posts'
+    },
+]
+
+const Links = () => {
+    return (
+        <div>
+            Liunks
+        </div>
+    )
+}
+
+export default Links
